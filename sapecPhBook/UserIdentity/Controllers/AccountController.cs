38149bde17ec97024b49910f1563659a18f20a66
stdg3 <velikoglosv@mail.ru>
2022-07-08 03:41:40 +0300

    don net identity

---
+++ sapecPhBook/UserIdentity/Controllers/AccountController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserIdentity.Identity;
+using UserIdentity.Models;
+
+namespace UserIdentity.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> userManager;
+
+        public AccountController()
+        {
+            var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
+            userManager = new UserManager<ApplicationUser>(userStore);
+
+        }
+        // GET: Account
+        //[AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = userManager.Find(model.Username, model.Password);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Incorrect login");
+                }
+                else
+                {
+                    var authManager = HttpContext.GetOwinContext().Authentication;
+
+                    var identity = userManager.CreateIdentity(user, "ApplicationCookie");
+
+                    var authProperties = new AuthenticationProperties()
+                    {
+                        IsPersistent = true
+                    };
+                    authManager.SignOut();
+                    authManager.SignIn(authProperties, identity);
+
+                    return Redirect(string.IsNullOrEmpty(returnUrl) ? "/" : returnUrl);
+                }
+            }
+
+            ViewBag.returnUrl = returnUrl;
+            return View(model);
+        }
+
+        [AllowAnonymous] // login olmadan da gozuken web sayfasi
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser();
+                user.UserName = model.Username;
+                user.Email = model.Email;
+                //user.Name = model.name
+
+                var result = userManager.Create(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item);
+                    }
+                }
+
+            }
+            return View(model);
+        }
+        public ActionResult Logout()
+        {
+            var authManager = HttpContext.GetOwinContext().Authentication;
+            authManager.SignOut();
+            return Redirect("/"); //RedirectToAction("Login");
+        }
+    }
+}
